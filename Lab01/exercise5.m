98e190f6183862d3da9fdec6cb52f7265416253e
alpondith <sujonalimran@gmail.com>
2025-02-24 10:39:11 +0100

    exercise 5 solution

---
+++ Lab01/exercise5.m
@@ -0,0 +1,34 @@
+% Generate a vector of random floating numbers between 0 and 1
+x = rand(1,10);
+
+% functions myMin which returns the minimum value and its index in the vector
+function [minimum, index_min] = myMin(x)
+    minimum = x(1);
+    index_min = 1;
+    for i = 2:length(x)
+        if x(i) < minimum
+            minimum = x(i);
+            index_min = i;
+        end
+    end
+end
+
+% function myMax which returns the maximum value and its index in the vector
+function [maximum, index_max] = myMax(x)
+    maximum = x(1);
+    index_max = 1;
+    for i = 2:length(x)
+        if x(i) > maximum
+            maximum = x(i);
+            index_max = i;
+        end
+    end
+end
+
+% Find the maximum and minimum values in the vector and the corresponding indices
+[minimum, index_min] = myMin(x);
+[maximum, index_max] = myMax(x);
+
+% Display the results
+fprintf('Minimum value: %f at index %d\n', minimum, index_min);
+fprintf('Maximum value: %f at index %d\n', maximum, index_max);
