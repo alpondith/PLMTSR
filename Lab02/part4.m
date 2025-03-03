719039079c81d53b14875063d10eb8b8dcbdee6b
alpondith <sujonalimran@gmail.com>
2025-03-03 01:27:28 +0100

    lab2 solution

---
+++ Lab02/part4.m
@@ -0,0 +1,32 @@
+% 64-QAM: Quadrature Amplitude Modulation, a type of digital modulation where
+% 64 different phases and amplitudes are used to represent 6-bit symbols.
+
+% number of bits to transmit
+n = 6000;
+% signal to noise ratio
+SNR = 16;
+% BPSK modulation order (number of symbols)
+modulation_order = 64; 
+% generate random bit stream
+bit_stream = randi([0, modulation_order-1],1,n);
+
+% calculate and display bits per symbol based on modulation order
+bits_per_symbol = log2(modulation_order);
+disp(['Bits per Symbol: ', num2str(bits_per_symbol)]);
+
+% modulate the bit stream
+modulated_signal = qammod(bit_stream,modulation_order);
+% scatter plot the modulated signal
+scatterplot(modulated_signal), title('Transmitted Signal');
+
+% received signal with AWGN noise
+received_signal = awgn(modulated_signal, SNR); 
+% scatter plot the received signal
+scatterplot(received_signal), title('Received Signal');
+
+% demodulation of the received signal 
+demodulated_bit_stream = pskdemod(received_signal, modulation_order); 
+
+% Calculate and display the number of bit errors
+error_bits = sum(demodulated_bit_stream ~= bit_stream);
+disp(['Error Bits: ', num2str(error_bits)]);
