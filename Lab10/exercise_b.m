ebe922e9d6acaaaea497923744a416d69b3d1aad
alpondith <sujonalimran@gmail.com>
2025-04-30 15:18:15 +0200

    solution

--- Lab10/exercise_b.m
+++ Lab10/exercise_b.m
@@ -1,71 +1,100 @@
-% filepath: /home/alpondith/projects/PLMTSR/Lab10/exercise01.m
-% Configuration Parameters
+clc;
+clear;
+close all;
 
-%% Setting Parameters
-Subcarriers = 64;                            % total number of subcarrier (IFFT length equal to Subcarriers)
-M = 16;                                      % number of constellations
-k = log2(M);                                 % number of bits per constellation
-numOfSym = 10^3;                             % number of OFDM Symbols
-GI = 1/4;                                    % Guard Interval or Cyclic Prefix, normally 25% of the entire OFDM symbols
-snrVec = -20:2:20;                           % SNR values from -20dB to +20dB
-ber = zeros(1, length(snrVec));              % Initialize BER array
-ber_theo = zeros(1, length(snrVec));         % Initialize theoretical BER array
+%% Parameters
+Subcarriers = 64;                % Total number of subcarriers
+numOfSym = 10^4;                 % Number of OFDM symbols
+GI = 1/4;                        % Guard Interval (Cyclic Prefix)
+snrVec = -20:2:20;               % SNR values from -20dB to +20dB
+ber = zeros(4, length(snrVec));  % BER for 4 users
+ber_theo = zeros(4, length(snrVec)); % Theoretical BER for 4 users
 
-%% --------------------- TRANSMITTER --------------------------------------
-%%------------------------------------------------------------------------
-%% Generate Data to be modulated on the subcarriers
-TxData = randi([0, M-1], Subcarriers, numOfSym);                             
- 
-%% Implement QAM modulation
-TxData_Modulated = qammod(TxData, M);
- 
-%% Perform IFFT
-TxData_IFFT = ifft(TxData_Modulated);
- 
-%% Adding cyclic Prefix
+% Modulation schemes for each user
+modulation_orders = [4, 16, 64, 256]; % 4-QAM, 16-QAM, 64-QAM, 256-QAM
+k = log2(modulation_orders);          % Bits per symbol for each modulation
+
+% Subcarrier allocation for each user
+user_subcarriers = {
+    1:16,   % User 1
+    17:32,  % User 2
+    33:48,  % User 3
+    49:64   % User 4
+};
+
+%% Generate Data for Each User
+TxData = cell(1, 4);
+for user = 1:4
+    TxData{user} = randi([0, modulation_orders(user)-1], length(user_subcarriers{user}), numOfSym);
+end
+
+%% Transmitter
+TxData_Modulated = cell(1, 4);
+for user = 1:4
+    TxData_Modulated{user} = qammod(TxData{user}, modulation_orders(user), 'UnitAveragePower', true);
+end
+
+% Combine all users' data into 64 subcarriers
+TxData_Combined = zeros(Subcarriers, numOfSym);
+for user = 1:4
+    TxData_Combined(user_subcarriers{user}, :) = TxData_Modulated{user};
+end
+
+% Perform IFFT
+TxData_IFFT = ifft(TxData_Combined);
+
+% Add Cyclic Prefix
 TxData_GI = [TxData_IFFT((1-GI)*Subcarriers+1:end, :); TxData_IFFT];
- 
-%% Plotting OFDM signal in time domain 
-[row, col] = size(TxData_GI);
-len = row * col;
-ofdm_signal = reshape(TxData_GI, 1, len);
-figure(1);
-plot(real(ofdm_signal)); 
-xlabel('Time'); 
-ylabel('Amplitude');
-title('OFDM Signal');
-grid on;
 
-%% --------------------- SIMULATION FOR DIFFERENT SNR VALUES --------------
+%% Channel and Receiver
 for i = 1:length(snrVec)
     snr = snrVec(i); % Current SNR value
     
-    %% Channel: Add AWGN noise
+    % Add AWGN noise
     rx_signal = awgn(TxData_GI, snr, 'measured');
     
-    %% --------------------- RECEIVER ----------------------------------------
-    %% CP removal
-    Recieve_GIremoved = rx_signal(GI*Subcarriers+1 : Subcarriers+GI*Subcarriers, :); 
-
-    %% FFT operation
+    % Remove Cyclic Prefix
+    Recieve_GIremoved = rx_signal(GI*Subcarriers+1 : Subcarriers+GI*Subcarriers, :);
+    
+    % Perform FFT
     RecieveData_FFT = fft(Recieve_GIremoved);
-
-    %% Demodulation
-    RecieveData = qamdemod(RecieveData_FFT, M);
-
-    %% Number of Bit Errors and Bit Error Rate computation
-    [~, BER] = biterr(TxData, RecieveData);
-    ber(i) = BER; % Store simulated BER for current SNR
-
-    %% Theoretical BER calculation
-    EbNoVec = snr - 10*log10(k); % Convert SNR to Eb/No
-    ber_theo(i) = berawgn(EbNoVec, 'qam', M); % Theoretical BER
+    
+    % Demodulate and calculate BER for each user
+    for user = 1:4
+        RecieveData_User = RecieveData_FFT(user_subcarriers{user}, :);
+        RecieveData_Demod = qamdemod(RecieveData_User, modulation_orders(user), 'UnitAveragePower', true);
+        
+        % Calculate BER
+        [~, BER] = biterr(TxData{user}, RecieveData_Demod);
+        ber(user, i) = BER;
+        
+        % Calculate Theoretical BER
+        EbNoVec = snr - 10*log10(k(user)); % Convert SNR to Eb/No
+        ber_theo(user, i) = berawgn(EbNoVec, 'qam', modulation_orders(user));
+    end
 end
 
-%% --------------------- PLOT BER RESULTS ---------------------------------
-figure 
-semilogy(snrVec, ber,'-ok', snrVec, ber_theo); 
-grid; 
-ylabel('BER'); 
-xlabel('SNR [dB]'); 
-legend('AWGN-simulation', 'AWGN-theoretical'); 
+%% Plot BER Curves for Each User
+figure;
+for user = 1:4
+    semilogy(snrVec, ber(user, :), '-o', 'DisplayName', ['User ', num2str(user), ' (Simulated)']);
+    hold on;
+    semilogy(snrVec, ber_theo(user, :), '--', 'DisplayName', ['User ', num2str(user), ' (Theoretical)']);
+end
+grid on;
+xlabel('SNR [dB]');
+ylabel('BER');
+title('BER vs. SNR for Different Users');
+legend('Location', 'southwest');
+
+%% Compare Users in Terms of BER
+figure;
+for user = 1:4
+    semilogy(snrVec, ber(user, :), '-o', 'DisplayName', ['User ', num2str(user)]);
+    hold on;
+end
+grid on;
+xlabel('SNR [dB]');
+ylabel('BER');
+title('Comparison of BER for Different Users');
+legend('Location', 'southwest');
